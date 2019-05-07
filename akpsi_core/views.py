b4a54c1d9da9451aecdb4a6ba89bc76f7a1d363e
jprechac <jprechac@gmail.com>
2019-05-07 18:06:33 -0500

    began configuring views and urls

--- akpsi_core/views.py
+++ akpsi_core/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
+class HomeView(TemplateView):
+    """
+    Landing page for the app.
+    """
+    template_name = 'akpsi_core/base.html'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        """
+        This function will expand the information that can be sent to the
+        template to use in the in-line python.
+        You can send objects, lists, dataframes, integers, pretty much whatever.
+        """
+        context = super().get_context_data(**kwargs)
+        return context
