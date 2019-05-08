82fb669bb095c4482cde58325066f427090245f8
jprechac <jprechac@gmail.com>
2019-05-07 21:31:30 -0500

    Register core models in admin site

--- akpsi_core/admin.py
+++ akpsi_core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import (
+    Area, Region, Semester, University, Chapter, College, Member
+)
 
 # Register your models here.
+admin.site.register(Area)
+admin.site.register(Region)
+admin.site.register(University)
+admin.site.register(Chapter)
+admin.site.register(College)
+admin.site.register(Member)
+admin.site.register(Semester)
