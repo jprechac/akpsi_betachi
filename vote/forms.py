c939dfdfc10d328a820c7c501afb7c8c03931232
bobbigraceh <50389458+bobbigraceh@users.noreply.github.com>
2019-05-16 18:43:39 -0400

    Update forms.py

--- vote/forms.py
+++ vote/forms.py
@@ -1,2 +1,23 @@
 from django import forms
-# from .models import
+from .models import Vote
+
+
+class VotingForm(forms.Form):
+    chosen_members_options = forms.MultipleChoiceField(choices=[], label='AKPsi', required=False,
+                                                     widget=forms.SelectMultiple(
+                                                        attrs={
+                                                             'class': 'form-control'
+                                                         }
+                                                     ))
+    other_member_name = forms.CharField(label='Other', max_length=100, required=False,
+                                      widget=forms.TextInput(
+                                        attrs={
+                                              'class': 'form-control',
+                                              'placeholder': 'Anybody else you would like to nominate?'
+                                          }
+                                      ))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        unique_members_names = Vote.objects.order_by('member_name').values_list('member_name', flat=True).distinct()
+        self.fields['chosen_members_options'].choices = [(member_name, member_name) for member_name in unique_member_names]# from .models import
