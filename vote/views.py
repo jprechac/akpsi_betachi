54430e7817135f9f127ff3004e020aa54f991b57
bobbigraceh <50389458+bobbigraceh@users.noreply.github.com>
2019-05-16 18:50:18 -0400

    Update views.py

--- vote/views.py
+++ vote/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import VotingForm
+from .models import Vote
+def index(request):
+if request.method == 'POST':
+        form = VotingForm(request.POST)
+if form.is_valid():
+            chosen_members_options = form.cleaned_data.get('chosen_members_options', [])
+            other_member_name = form.cleaned_data.get('other_member_name', '')
+            Vote.bulk_vote(chosen_members_options + [other_member_name])
+        message = 'Thank You For Voting!'
+elif request.method == 'GET':
+        message = ''
+    form = VotingForm()
+return render(request, 'templates/survey.html', {'form': form, 'message': message})# Create your views here.
