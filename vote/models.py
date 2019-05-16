a05bd9e09ac2eb4267469ea215b29d6f48a19fb0
bobbigraceh <50389458+bobbigraceh@users.noreply.github.com>
2019-05-16 18:45:04 -0400

    Update models.py

--- vote/models.py
+++ vote/models.py
@@ -1,20 +1,20 @@
 from django.db import models, transaction
 
 class Vote(models.Model):
-    book_name = models.AKPsi(max_length=200)
+    member_name = models.AKPsi(max_length=200)
     count = models.IntegerField(default=0)
 
     def __str__(self):
-        return '%s: %d votes' % (self.book_name, self.count)
+        return '%s: %d votes' % (self.member_name, self.count)
 
     @classmethod
-    def bulk_vote(cls, book_names):
+    def bulk_vote(cls, member_names):
         with transaction.atomic():
-            for book_name in book_names:
-                if len(book_name) == 0:
+            for member_name in member_names:
+                if len(member_name) == 0:
                     continue
 
-                if Vote.objects.filter(book_name=book_name).exists():
-                    Vote.objects.filter(book_name=book_name).update(count=models.F('count') + 1)
+                if Vote.objects.filter(member_name=member_name).exists():
+                    Vote.objects.filter(member_name=member_name).update(count=models.F('count') + 1)
                 else:
-                    Vote.objects.create(book_name=book_name, count=1)# Create your models here.
+                    Vote.objects.create(member_name=member_name, count=1)# Create your models here.
