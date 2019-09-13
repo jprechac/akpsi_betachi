0569e273044f290f1f0cf745c70cb85a5919e6ea
jprechac <jprechac@gmail.com>
2019-09-12 21:00:19 -0500

    update vote member relationship

--- vote/models.py
+++ vote/models.py
@@ -1,20 +1,21 @@
 from django.db import models, transaction
+from ..akpsi_core import models as core
 
 class Vote(models.Model):
-    member_name = models.AKPsi(max_length=200)
+    brother_id = models.ForeignKey(core.Member)
     count = models.IntegerField(default=0)
 
     def __str__(self):
-        return '%s: %d votes' % (self.member_name, self.count)
+        return '%s: %d votes' % (self.brother_id, self.count)
 
     @classmethod
-    def bulk_vote(cls, member_names):
+    def bulk_vote(cls, brother_ids):
         with transaction.atomic():
-            for member_name in member_names:
+            for brother_id in brother_ids:
                 if len(member_name) == 0:
                     continue
 
-                if Vote.objects.filter(member_name=member_name).exists():
-                    Vote.objects.filter(member_name=member_name).update(count=models.F('count') + 1)
+                if Vote.objects.filter(brother_id=brother_id).exists():
+                    Vote.objects.filter(brother_id=brother_id).update(count=models.F('count') + 1)
                 else:
-                    Vote.objects.create(member_name=member_name, count=1)# Create your models here.
+                    Vote.objects.create(brother_id=brother_id, count=1)# Create your models here.
