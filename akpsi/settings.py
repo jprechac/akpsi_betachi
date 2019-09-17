58bba02ac62a4526f7e62eb82b37688cc5c60527
jprechac <jprech4@lsu.edu>
2019-09-17 15:09:03 -0500

    fix error handling in get_env_variable

--- akpsi/settings.py
+++ akpsi/settings.py
@@ -21,9 +21,9 @@ def get_env_variable(name):
     """
     try:
         return os.environ[name]
-    except KeyError:
+    except KeyError as ke:
         message = '{0} is not an environment variable!'.format(name)
-        raise ImproperlyConfigured(message)
+        raise Exception(message)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
